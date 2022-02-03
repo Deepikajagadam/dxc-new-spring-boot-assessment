0292fda70083e3960ce443b3fec67f2e0ef13803
Deepikajagadam <96856837+Deepikajagadam@users.noreply.github.com>
2022-02-03 20:12:56 +0530

    Add files via upload

---
+++ author-rest-api/src/test/java/com/dxc/learning/authorrestapi/AuthorRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxc.learning.authorrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
